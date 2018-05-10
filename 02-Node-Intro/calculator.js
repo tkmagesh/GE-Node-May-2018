78a496cdaac197e03adb70f27fea236626d0e833
Magesh <tkmagesh77@gmail.com>
2018-05-10 14:09:06 +0530

    modularity

---
+++ 02-Node-Intro/calculator.js
@@ -0,0 +1,20 @@
+let calculator = {
+	add(x,y){
+		return x + y;
+	},
+	subtract(x,y){
+		return x - y;
+	},
+	multiply(x,y){
+		return x * y;
+	},
+	divide(){
+		return x / y;
+	}
+};
+
+let x = 100, y = 50;
+console.log(calculator.add(x,y));
+console.log(calculator.subtract(x,y));
+console.log(calculator.multiply(x,y));
+console.log(calculator.divide(x,y));
